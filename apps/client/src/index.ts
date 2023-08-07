32c7615db13710939a2118c310c149ad19789ccd
abdullahmorrison <abdullahmorrison@gmail.com>
2023-08-07 13:42:29 -0400

    set up workspaces (#8)

--- apps/client/src/index.ts
+++ apps/client/src/index.ts
@@ -28,7 +28,7 @@ async function main(){
     obtainmentTimestamp: 0
   }, ['chat'])
 }
-main()
+main().catch(console.error)
 
 function onConnectedHandler(addr: string, port: number) {
   console.log('\x1b[32m%s\x1b[0m', `* Connected to ${addr}:${port}`)
