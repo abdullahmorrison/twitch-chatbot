84d80a0162c9818356ce2e2297a5f2376ab12eed
abdullahmorrison <abdullahmorrison@gmail.com>
2023-08-09 10:48:01 -0400

    updated fetch url to production

--- apps/client/src/trpcClient.ts
+++ apps/client/src/trpcClient.ts
@@ -2,5 +2,5 @@ import  { createTRPCProxyClient, httpBatchLink } from '@trpc/client'
 import { AppRouter } from '../../server/server'
 
 export const client = createTRPCProxyClient<AppRouter>({
-    links: [ httpBatchLink({ url: 'http://localhost:3000' }) ]
+    links: [ httpBatchLink({ url: 'https://chatbot-server.up.railway.app' }) ]
 })
