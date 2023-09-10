12418b244ef893cf25ffe0f752f0d3c70c36da27
abdullahmorrison <abdullahmorrison@gmail.com>
2023-09-10 14:42:42 -0400

    client: update chatbot refresh

--- apps/client/src/chatbot.ts
+++ apps/client/src/chatbot.ts
@@ -3,7 +3,7 @@ import { ChatClient } from '@twurple/chat'
 import { ApiClient } from '@twurple/api';
 import { EventSubWsListener } from '@twurple/eventsub-ws';
 import { trpcClient } from './trpcClient'
-import { StaticAuthProvider, RefreshingAuthProvider } from '@twurple/auth'
+import { RefreshingAuthProvider } from '@twurple/auth'
 import { onConnectedHandler, onDisconnectedHandler, onMessageHandler, onStreamerOnline, onStreamerOffline} from './eventHandlers'
 import channels from './data/channels.json'
 
@@ -15,7 +15,21 @@ async function main(){
 
   const data = await trpcClient.accessToken.query()
   token = data[0].token
-  const authProvider = new StaticAuthProvider(process.env.TWITCH_CLIENT_ID as string, token)
+
+  const authProvider = new RefreshingAuthProvider({
+    clientId: process.env.TWITCH_CLIENT_ID as string,
+    clientSecret: process.env.TWITCH_CLIENT_SECRET as string,
+  })
+  await authProvider.addUserForToken({
+    accessToken: token,
+    refreshToken: process.env.TWITCH_REFRESH_TOKEN as string,
+    expiresIn: 0,
+    obtainmentTimestamp: 0
+  }, ['chat'])
+  authProvider.onRefresh(async (_, newTokenData)=>{
+    console.log('\x1b[36m%s\x1b[0m', 'Token Refreshed')
+    await trpcClient.accessTokenUpdate.mutate(newTokenData.accessToken)
+  })
 
   chatClient = new ChatClient({authProvider, channels: channels});
 
@@ -24,28 +38,7 @@ async function main(){
   chatClient.onDisconnect((_, reason) => onDisconnectedHandler(reason))
   await chatClient.connect()
 
-  const refreshAuthProvider = new RefreshingAuthProvider({
-    clientId: process.env.TWITCH_CLIENT_ID as string,
-    clientSecret: process.env.TWITCH_CLIENT_SECRET as string,
-  })
-  await refreshAuthProvider.addUserForToken({
-    accessToken: token,
-    refreshToken: process.env.TWITCH_REFRESH_TOKEN as string,
-    expiresIn: null,
-    obtainmentTimestamp: 0
-  }, ['chat'])
-  refreshAuthProvider.onRefresh(async ()=>{
-    console.log('\x1b[36m%s\x1b[0m', 'Refreshing token...')
-    const response = await fetch('https://id.twitch.tv/oauth2/token?grant_type=refresh_token&refresh_token=' 
-      + process.env.TWITCH_REFRESH_TOKEN 
-      + '&client_id=' + process.env.TWITCH_CLIENT_ID 
-      + '&client_secret=' + process.env.TWITCH_CLIENT_SECRET, 
-      { method: 'POST' })
-    const data = await response.json()
-    token = await trpcClient.accessTokenUpdate.mutate(data.access_token) as string
-  })
-
-  const apiClient = new ApiClient({ authProvider: authProvider})
+  const apiClient = new ApiClient({ authProvider })
   const eventListener = new EventSubWsListener({ apiClient })
   eventListener.start()
 
