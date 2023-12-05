c81120f8b188e582a7702522ff6659705b4844ac
abdullahmorrison <abdullahmorrison@gmail.com>
2023-12-05 12:23:50 -0500

    updated whyisbritttnotlive command when britt types

--- apps/client/src/commands.ts
+++ apps/client/src/commands.ts
@@ -82,7 +82,7 @@ const recipe = withCooldown(async (channel: string)=>{
 const whyIsBritttNotLive = withCooldown(async (channel: string, user: string)=>{
   setTimeout(()=>{
     if(user === 'brittt')
-      chatClient.say(channel, "Texime I'm typing in chat instead of going live (go live arnoldHalt )")
+      chatClient.say(channel, "Texime I'm typing in chat instead of going live (go live FeelsWeirdMan )")
     else
       chatClient.say(channel, "Texime "+whyisbritttnotlive[Math.floor(Math.random() * whyisbritttnotlive.length)])
   }, 2000)
