e87b5e512754a8752f55f00880be47b931681c0d
Abdullah Morrison <abdullahmorrison@gmail.com>
2024-02-24 18:25:16 -0500

    Update !brittwheel link

--- apps/client/src/commands.ts
+++ apps/client/src/commands.ts
@@ -133,7 +133,7 @@ const fortune = withCooldown(async (channel: string, user: string)=>{
 })
 const brittWheel = withCooldown(async (channel: string, user: string)=>{
   setTimeout(()=>{
-    chatClient.say(channel, "@Brittt DinkDonk https://pickerwheel.com/pw?id=AdAdv")
+    chatClient.say(channel, "@Brittt DinkDonk https://pickerwheel.com/pw?id=BEQax")
   }, 2000)
 })
 
